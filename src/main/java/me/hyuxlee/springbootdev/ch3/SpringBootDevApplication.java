0a51346b14a5a77f22b20df5e133c59dd009b94e
hyxklee <ewgt1234@naver.com>
2023-12-08 21:06:55 +0900

    http db study

--- src/main/java/me/hyuxlee/springbootdev/ch3/SpringBootDevApplication.java
+++ src/main/java/me/hyuxlee/springbootdev/ch3/SpringBootDevApplication.java
@@ -1,4 +1,4 @@
-package me.hyuxlee.springbootdev;
+package me.hyuxlee.springbootdev.ch3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
