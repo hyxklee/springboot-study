37c09f29403598101aac08a55854f56af6ec9220
hyxklee <ewgt1234@naver.com>
2023-12-08 17:44:53 +0900

    first commit

---
+++ src/main/java/me/hyuxlee/springbootdev/SpringBootDevApplication.java
@@ -0,0 +1,11 @@
+package me.hyuxlee.springbootdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootDevApplication.class, args);
+    }
+}
