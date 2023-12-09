94a3ca526d9f3a75bbc69eec307a6479d3d2bf4b
hyxklee <ewgt1234@naver.com>
2023-12-09 15:21:22 +0900

    test study commit

---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,9 @@
+/*import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    @DisplayName("1+3는 4이다")//테스트 이름
+    @Test//테스트 메서드
+
+}*/
